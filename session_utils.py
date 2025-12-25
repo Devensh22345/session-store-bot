752168a736e1686639f152ac476f1c5d35a32823
Devensh22345 <kumardevensh0@gmail.com>
2025-12-25 20:39:05 +0530

    Update session_utils.py

--- session_utils.py
+++ session_utils.py
@@ -1,7 +1,7 @@
-import base64
 from pyrogram import Client
 from pyrogram.errors import SessionPasswordNeeded
 from datetime import datetime
+import base64
 
 async def generate_session(api_id, api_hash, phone, otp, password=None):
     app = Client(
@@ -16,6 +16,9 @@ async def generate_session(api_id, api_hash, phone, otp, password=None):
     try:
         await app.sign_in(phone, otp)
     except SessionPasswordNeeded:
+        if not password:
+            await app.disconnect()
+            raise SessionPasswordNeeded
         await app.check_password(password)
 
     me = await app.get_me()
@@ -32,6 +35,7 @@ async def generate_session(api_id, api_hash, phone, otp, password=None):
         "created_at": datetime.utcnow(),
         "valid": True
     }
+
 
 async def check_valid(api_id, api_hash, session_string):
     try:
