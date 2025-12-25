2f5c2384e6189a308e17577f400408614e1baf50
Devensh22345 <kumardevensh0@gmail.com>
2025-12-25 20:59:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ import os
 # =====================
 # BOT CONFIG
 # =====================
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-OWNER_ID = int(os.getenv("OWNER_ID", "0"))
+BOT_TOKEN = os.getenv("BOT_TOKEN" ,"7611358375:AAET1Zs1DSNhuSeXhJfDvc6GFhOZn5YgC5Q")
+OWNER_ID = int(os.getenv("OWNER_ID", "6872968794"))
 
 # =====================
 # DATABASE
 # =====================
-MONGO_URI = os.getenv("MONGO_URI")
+MONGO_URI = os.getenv("MONGO_URI",  "mongodb+srv://5:5@cluster0.otqyz3n.mongodb.net/?appName=Cluster0")
 DB_NAME = os.getenv("DB_NAME", "SESSION_MANAGER")
 
 # =====================
